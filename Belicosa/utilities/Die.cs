a6045ccb38084109ba1eff866437afbc4454c85c
Nickolas Carlos <nickolascarlos@gmail.com>
2024-01-23 17:21:25 -0300

    feat: Implements Die

--- Belicosa/utilities/Die.cs
+++ Belicosa/utilities/Die.cs
@@ -8,6 +8,14 @@ namespace Belicosa.utilities
 {
     class Die
     {
-        
+        public static int Throw()
+        {
+            return new Random().Next(1, 7);
+        }
+
+        public static List<int> Throw(int throwsCount)
+        {
+            return (from _ in Enumerable.Range(1, throwsCount) select Throw()).ToList();
+        }
     }
 }
